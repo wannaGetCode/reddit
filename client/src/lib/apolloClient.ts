809ee1c35a8a62cb1e8808e7f073a9648f1a91d2
wannaGetCode <ducquang2503@gmail.com>
2022-05-18 16:30:53 +0700

    change uri

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -31,7 +31,7 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
   }
 
   const httpLink = new HttpLink({
-    uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
+    uri: process.env.NODE_ENV === 'production' ? 'https://redd1t.herokuapp.com/graphql' : 'http://localhost:4000/graphql', // Server URL (must be absolute)
     credentials: 'include', // Additional fetch() options like `credentials` or `headers`
     fetch: enhancedFetch
   })
